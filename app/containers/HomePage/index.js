fafc04ca404a81cec28a6fb1611f19c7a6895ad0
andrepestana92 <andre.pest92@gmail.com>
2018-04-09 23:18:27 -0300

    Changed project name for PWA

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,14 +11,14 @@
 
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import AppNavBar from '../AppNavBar';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div>
+        <AppNavBar history={this.props.history} />
+      </div>
     );
   }
 }
